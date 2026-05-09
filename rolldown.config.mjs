1db35561d5f2aae8fe29236906d830c4b6e5de23
Josh Vlk <josh@vlkpack.com>
2026-05-09 17:39:32 -0400

    minify

--- rolldown.config.mjs
+++ rolldown.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from "rolldown"
+import { defineConfig } from "rolldown";
 
 export default defineConfig({
-  input: "src/Main.res.mjs",
-  platform: "node",
-  external: [/^node:/],
-  output: {
-    file: "bin/index.mjs",
-    format: "esm",
-    banner: () => "#!/usr/bin/env node",
-  },
-})
+    input: "src/Main.res.mjs",
+    platform: "node",
+    external: [/^node:/],
+    treeshake: true,
+    output: {
+        file: "bin/index.mjs",
+        format: "esm",
+        banner: () => "#!/usr/bin/env node",
+        minify: true,
+    },
+});
